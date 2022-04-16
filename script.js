bcbe07c3a9ea9279f3fd83fed36d250f9f7bad6e
taepal467 <chantae.palacio.38@my.csun.edu>
2022-04-16 10:48:09 -0700

    Incomplete

---
+++ script.js
@@ -0,0 +1,34 @@
+//FORM
+let form = document.getElementById('form');
+
+//INPUTS
+let billInput = document.getElementsByClassName('bill-input')[0];
+let numberOfPeopleInput = document.getElementsByClassName('numOfPeople-input')[0];
+let customInput = document.getElementsByClassName("custom-input")[0];
+let amountInput = document.getElementsByClassName('amount-input')[0];
+let totalInput = document.getElementsByClassName('total-input')[0];
+
+//BUTTONS
+let selectTipButtons = form.getElementsByClassName('tip-buttons');
+let resetButton = document.getElementsByClassName('reset-button')[0];
+
+//ERROR MESSAGE
+let errorMessage = document.getElementsByClassName('error-message')[0];
+
+//VALUES
+let billAmount = billInput.value;
+let numberOfPeople = numberOfPeopleInput.value;
+let customTip = customInput.value;
+let buttonValues = selectTipButtons.value;
+
+
+
+   
+  
+
+
+
+
+
+
+
